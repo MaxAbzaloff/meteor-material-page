d2dbb417502762d04c11023b5452ba693303e991
Max Abzaloff <max.abzaloff@gmail.com>
2018-02-10 20:09:52 +1000

    add scss

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,12 +2,18 @@ import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 import ContentContainer from './ContentContainer'
+import SideMenuContainer from './SideMenuContainer'
+import ChatContainer from './ChatContainer'
 
 class App extends React.Component {
   render () {
     return (
       <MuiThemeProvider>
-        <ContentContainer />
+        <React.Fragment>
+          <SideMenuContainer />
+          <ContentContainer />
+          <ChatContainer />
+        </React.Fragment>
       </MuiThemeProvider>
     )
   }
---
+++ imports/ui/SideMenuContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SideMenuContainer extends Component {
+  render () {
+    return (
+      <div>
+        SideMenuContainer
+      </div>
+    )
+  }
+}
+
+export default SideMenuContainer
