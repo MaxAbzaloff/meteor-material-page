7c1cd5501d5214cf0e2baf15c937c6bd025b5e62
Max Abzaloff <max.abzaloff@gmail.com>
2018-03-06 05:17:45 +1000

    tasks are ready

--- imports/ui/components/Task.js
+++ imports/ui/components/Task.js
@@ -11,42 +11,45 @@ const buttonStyles = {
 }
 
 const Task = ({task}) => (
-  <div className={'task ' + 'task__priority_' + task.priority.toLowerCase()}>
-    <div className='task__more'>
-      <i className='material-icons'>more_vert</i>
-    </div>
+  <React.Fragment>
+    <div className={'task ' + 'task__priority_' + task.priority.toLowerCase()}>
+      <div className='task__more'>
+        <i className='material-icons'>more_vert</i>
+      </div>
 
-    <div className='task__content'>
-      <span className='task__tag'>{task.tag}</span><br />
-      <h4 className='task__title'>{task.title}</h4><br />
+      <div className='task__content'>
+        <span className='task__tag'>{task.tag}</span><br />
+        <h4 className='task__title'>{task.title}</h4><br />
 
-      <div className='task__details'>
-        <div className={'task__flag task__flag_priority_' + task.priority.toLowerCase()}>
-          <span className='task__point' />
-          {task.priority}
+        <div className='task__details'>
+          <div className={'task__flag task__flag_priority_' + task.priority.toLowerCase()}>
+            <span className='task__point' />
+            {task.priority}
+          </div>
+          <div className='task__flag'>
+            <span className='task__point' />
+            {task.status}
+          </div>
+          {/* <CustomAvatar src={task.avatar} className='task__avatar' /> */}
+          <span className='task__avatar' />
+          <span className='task__detail'>{task.employee}</span>
+          <span className='task__detail'>{'Due date: ' + task.dueDate}</span>
+          <span className='task__detail'>{'Estimated: ' + task.estimated}</span>
         </div>
-        <div className='task__flag'>
-          <span className='task__point' />
-          {task.status}
-        </div>
-        {/* <CustomAvatar src={task.avatar} className='task__avatar' /> */}
-        <span className='task__avatar' />
-        <span className='task__detail'>{task.employee}</span>
-        <span className='task__detail'>{'Due date: ' + task.dueDate}</span>
-        <span className='task__detail'>{'Estimated: ' + task.estimated}</span>
       </div>
-    </div>
 
-    <div className='log-data task__log-data'>
-      <div className='log-data__block log-data__block_get-lower_0-2-rem'>
-        <span className='log-data__logged'>Logged {task.logged}</span>
-        <LinearProgress mode='determinate' value={task.completed} />
-      </div>
-      <div className='log-data__block log-data__block_get-higer_0-7-rem'>
-        <RaisedButton label='Log Time' labelPosition='before' icon={<i className='material-icons'>timer</i>} labelColor='white' buttonStyle={buttonStyles} backgroundColor='#5da6ff' />
+      <div className='log-data task__log-data'>
+        <div className='log-data__block log-data__block_get-lower_0-2-rem'>
+          <span className='log-data__logged'>Logged {task.logged}</span>
+          <LinearProgress mode='determinate' value={task.completed} />
+        </div>
+        <div className='log-data__block log-data__block_get-higer_0-7-rem'>
+          <RaisedButton label='Log Time' labelPosition='before' icon={<i className='material-icons'>timer</i>} labelColor='white' buttonStyle={buttonStyles} backgroundColor='#5da6ff' />
+        </div>
       </div>
     </div>
-  </div>
+    <div className='gray-container' />
+  </React.Fragment>
 )
 
 export default Task
--- imports/ui/components/Tasks.js
+++ imports/ui/components/Tasks.js
@@ -8,14 +8,16 @@ import './Tasks.scss'
 
 const Tasks = ({tasks = []}) => (
   <div className='tasks'>
-    <h3 className='tasks__header'>Tasks</h3>
-    <CustomIconButton className='tasks__icon-button' />
-    <div className='tasks__filters'>
-      <span>Priority<i className='material-icons tasks__filter-icon'>arrow_drop_down</i></span>
-      <span>Asignee<i className='material-icons tasks__filter-icon'>arrow_drop_down</i></span>
-      <span>Recent<i className='material-icons tasks__filter-icon'>arrow_drop_down</i></span>
+    <div className='tasks__control-panel'>
+      <h3 className='tasks__header'>Tasks</h3>
+      <CustomIconButton className='tasks__icon-button' />
+      <div className='tasks__filters'>
+        <span>Priority<i className='material-icons tasks__filter-icon'>arrow_drop_down</i></span>
+        <span>Asignee<i className='material-icons tasks__filter-icon'>arrow_drop_down</i></span>
+        <span>Recent<i className='material-icons tasks__filter-icon'>arrow_drop_down</i></span>
+      </div>
+      <span className='tasks__details'>This is your tasks sheet. Here you can create, delete and edit tasks concerning your project.</span>
     </div>
-    <span className='tasks__details'>This is your tasks sheet. Here you can create, delete and edit tasks concerning your project.</span>
     <div className='tasks__stack'>
       {
         tasks.map(task => <Task key={task.id} task={task} />)
